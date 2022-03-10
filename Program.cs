d016618af4a922c3b55c2faa8726e28fbdea4964
Bernard <72697668+Vires404@users.noreply.github.com>
2022-03-10 20:03:57 +0800

    a test game

--- Program.cs
+++ Program.cs
@@ -12,11 +12,31 @@ namespace project
     {
         static void Main()
         {
-            Random coin = new Random();
-            // int flip = coin.Next(0,2);
-            Console.WriteLine((coin.Next(0,2) == 0)? "heads" : "tails");
-            
+            int hero_Hp = 10;
+int monster_Hp = 10;
+Random damage_Hero = new Random();
+Random damage_Monster = new Random();
+do{
+    int hero_Attack = damage_Hero.Next(1,11);
+    int monster_Attack = damage_Monster.Next(1,11);
+    hero_Hp -= monster_Attack;
+    monster_Hp -= hero_Attack;
+    Console.WriteLine($"Monster was damaged and lost {hero_Attack} health and now has {monster_Hp} health.");
+    Console.WriteLine($"Hero was damaged and lost {monster_Attack} health and now has {hero_Hp} health.");
+    if(hero_Hp <= 0)
+    {
+        Console.WriteLine("monster wins");
+        break;
+    }
+    else if(monster_Hp <= 0)
+    {
+        Console.WriteLine("hero wins");
+        break;
+    }
+}while((hero_Hp>0)&&(monster_Hp>0));
         }
+
+       
             
             
     }
