79b940f1c8019719240b1e4b1caa2330818e1780
Bernard <72697668+Vires404@users.noreply.github.com>
2022-03-10 16:32:35 +0800

    coins

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace project
+{
+    class Program
+    {
+        static void Main()
+        {
+            Random coin = new Random();
+            // int flip = coin.Next(0,2);
+            Console.WriteLine((coin.Next(0,2) == 0)? "heads" : "tails");
+            
+        }
+            
+            
+    }
+    
+    
+    
+}
